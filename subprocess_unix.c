afa44bda49019ea70297ab20ac9408db9b04dca4
Aaron Small <aaron.small@shadnet.shad.ca>
2010-02-06 21:55:18 -0500

    Put in some temporary sample code for subprocess on linux

--- subprocess_unix.c
+++ subprocess_unix.c
@@ -1,2 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <sys/ptrace.h>
+#include <asm/ptrace-abi.h>
 #include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	long orig_eax;
+	pid_t child;
+
+	child = fork();
+	if( child == 0 ) {
+		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+		execl("/bin/ls", "ls", NULL);
+	} else {
+		wait(NULL);
+		orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
+		printf("The child made a system call %ld\n", orig_eax);
+		ptrace(PTRACE_CONT, child, NULL, NULL);
+	}
+	return 0;
+}
