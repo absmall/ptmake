b30054179fdfb7c5158ccc768c9bf169c3e91423
Aaron Small <aaron.small@shadnet.shad.ca>
2010-02-07 17:16:34 -0500

    Print out all syscalls made, not just the first

--- subprocess_unix.c
+++ subprocess_unix.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/ptrace.h>
@@ -7,18 +8,28 @@
 
 int main(int argc, char *argv[])
 {
+	int status;
 	long orig_eax;
 	pid_t child;
 
 	child = fork();
 	if( child == 0 ) {
 		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-		execl("/bin/ls", "ls", NULL);
+		if( argc >= 2 ) {
+			execl(argv[1], argv[1], NULL);
+		} else {
+			execl("/bin/ls", "ls", NULL);
+		}
 	} else {
-		wait(NULL);
-		orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
-		printf("The child made a system call %ld\n", orig_eax);
-		ptrace(PTRACE_CONT, child, NULL, NULL);
+		while(1) {
+			wait(&status);
+			if(WIFEXITED(status)) break;
+			orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
+			if( orig_eax == 5 ) {
+				printf("The child made a system call %ld\n", orig_eax);
+			}
+			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+		}
 	}
 	return 0;
 }
