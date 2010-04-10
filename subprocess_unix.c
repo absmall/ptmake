9a48a27f8be6c8f4b16247041f0a9f549bc0450f
Aaron Small <a.small@unb.ca>
2010-04-10 19:04:20 -0400

    Print out only the syscalls I think might be interesting. Add a file I forgot to add earlier

--- subprocess_unix.c
+++ subprocess_unix.c
@@ -5,6 +5,7 @@
 #include <sys/ptrace.h>
 #include <sys/reg.h>
 #include <sys/wait.h>
+#include <sys/syscall.h>
 
 #include "subprocess.h"
 
@@ -29,9 +30,50 @@ void trace(const char *command)
 #elif defined(__x86_64)
 			ret = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
 #endif
-			printf("The child made a system call %ld\n", ret);
-			if( ret == 5 ) {
-				printf("The child made a system call %ld\n", ret);
+			switch( ret ) {
+				case __NR_stat:
+					printf("Stat call\n");
+					break;
+				case __NR_fstat:
+					printf("fstat call\n");
+					break;
+				case __NR_open:
+					printf("Open call\n");
+					break;
+				case __NR_access:
+					printf("Access call\n");
+					break;
+				case __NR_fcntl:
+					printf("Fcntl call\n");
+					break;
+				case __NR_arch_prctl:
+					printf("Arch prctl call\n");
+					break;
+				case __NR_execve:
+					printf("Execve call\n");
+					break;
+				case __NR_dup2:
+					printf("Dup2 call\n");
+					break;
+				case __NR_exit_group:
+					printf("Exit group call\n");
+					break;
+				// Don't care about these ones
+				case __NR_mmap:
+				case __NR_mprotect:
+				case __NR_getppid:
+				case __NR_brk:
+				case __NR_geteuid:
+				case __NR_getpid:
+				case __NR_munmap:
+				case __NR_read:
+				case __NR_write:
+				case __NR_rt_sigaction:
+				case __NR_close:
+					break;
+				default:
+					printf("The child made a system call %ld\n", ret);
+					break;
 			}
 			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 		}
