e9f9e00c12fca8fe8d7df001b58ae4eff767dd58
Aaron Small <aaron.small@shadnet.shad.ca>
2010-09-26 14:46:03 -0400

    Give the full set of rules to build the current project on unix

--- static_rules.cc
+++ static_rules.cc
@@ -2,7 +2,43 @@
 
 void static_rules()
 {
-		Rule *r = new Rule();
-		r->addTarget("main.o");
-		r->addCommand("g++ -c -Wall -DDEBUG -g3 -o main.o main.cc");
+		Rule *make = new Rule();
+		make->addTarget("make.exe");
+		make->addCommand("g++ -DYYDEBUG -g3 -o make.exe main.o build.o find.o argpc.o argpcoption.o exception.o rules.o static_rules.o subprocess_unix.o");
+
+		Rule *main = new Rule();
+		main->addTarget("main.o");
+		main->addCommand("g++ -c -Wall -DDEBUG -g3 -o main.o main.cc");
+
+		Rule *build = new Rule();
+		build->addTarget("build.o");
+		build->addCommand("g++ -c -Wall -DDEBUG -g3 -o build.o build.cc");
+
+		Rule *find = new Rule();
+		find->addTarget("find.o");
+		find->addCommand("g++ -c -Wall -DDEBUG -g3 -o find.o find.cc");
+
+		Rule *argpc = new Rule();
+		argpc->addTarget("argpc.o");
+		argpc->addCommand("g++ -c -Wall -DDEBUG -g3 -o argpc.o argpc.cc");
+
+		Rule *argpcoption = new Rule();
+		argpcoption->addTarget("argpcoption.o");
+		argpcoption->addCommand("g++ -c -Wall -DDEBUG -g3 -o argpcoption.o argpcoption.cc");
+
+		Rule *exception = new Rule();
+		exception->addTarget("exception.o");
+		exception->addCommand("g++ -c -Wall -DDEBUG -g3 -o exception.o exception.cc");
+
+		Rule *rules = new Rule();
+		rules->addTarget("rules.o");
+		rules->addCommand("g++ -c -Wall -DDEBUG -g3 -o rules.o rules.cc");
+
+		Rule *static_rules = new Rule();
+		static_rules->addTarget("static_rules.o");
+		static_rules->addCommand("g++ -c -Wall -DDEBUG -g3 -o static_rules.o static_rules.cc");
+
+		Rule *subprocess_unix = new Rule();
+		subprocess_unix->addTarget("subprocess_unix.o");
+		subprocess_unix->addCommand("g++ -c -Wall -DDEBUG -g3 -o subprocess_unix.o subprocess_unix.cc");
 }
