054b05b98d820c246da3e8577a590eda19be702c
Aaron Small <aaron.small@shadnet.shad.ca>
2010-11-07 21:23:49 -0500

    Fix indentation

--- static_rules.cc
+++ static_rules.cc
@@ -2,43 +2,53 @@
 
 void static_rules()
 {
-		Rule *make = new Rule();
-		make->addTarget("make.exe");
-		make->addCommand("g++ -DYYDEBUG -g3 -o make.exe main.o build.o find.o argpc.o argpcoption.o exception.o rules.o static_rules.o subprocess_unix.o");
+#if 0
+	Rule *make = new Rule();
+	make->addTarget("make.exe");
+	make->addCommand("./a.out b.out");
 
-		Rule *main = new Rule();
-		main->addTarget("main.o");
-		main->addCommand("g++ -c -Wall -DDEBUG -g3 -o main.o main.cc");
+	Rule *make2 = new Rule();
+	make2->addTarget("b.out");
+	make2->addCommand("./b.out");
+#endif
 
-		Rule *build = new Rule();
-		build->addTarget("build.o");
-		build->addCommand("g++ -c -Wall -DDEBUG -g3 -o build.o build.cc");
+	Rule *make = new Rule();
+	make->addTarget("make.exe");
+	make->addCommand("g++ -DYYDEBUG -g3 -o make.exe main.o build.o find.o argpc.o argpcoption.o exception.o rules.o static_rules.o subprocess_unix.o");
 
-		Rule *find = new Rule();
-		find->addTarget("find.o");
-		find->addCommand("g++ -c -Wall -DDEBUG -g3 -o find.o find.cc");
+	Rule *main = new Rule();
+	main->addTarget("main.o");
+	main->addCommand("g++ -c -Wall -DDEBUG -g3 -o main.o main.cc");
 
-		Rule *argpc = new Rule();
-		argpc->addTarget("argpc.o");
-		argpc->addCommand("g++ -c -Wall -DDEBUG -g3 -o argpc.o argpc.cc");
+	Rule *build = new Rule();
+	build->addTarget("build.o");
+	build->addCommand("g++ -c -Wall -DDEBUG -g3 -o build.o build.cc");
 
-		Rule *argpcoption = new Rule();
-		argpcoption->addTarget("argpcoption.o");
-		argpcoption->addCommand("g++ -c -Wall -DDEBUG -g3 -o argpcoption.o argpcoption.cc");
+	Rule *find = new Rule();
+	find->addTarget("find.o");
+	find->addCommand("g++ -c -Wall -DDEBUG -g3 -o find.o find.cc");
 
-		Rule *exception = new Rule();
-		exception->addTarget("exception.o");
-		exception->addCommand("g++ -c -Wall -DDEBUG -g3 -o exception.o exception.cc");
+	Rule *argpc = new Rule();
+	argpc->addTarget("argpc.o");
+	argpc->addCommand("g++ -c -Wall -DDEBUG -g3 -o argpc.o argpc.cc");
 
-		Rule *rules = new Rule();
-		rules->addTarget("rules.o");
-		rules->addCommand("g++ -c -Wall -DDEBUG -g3 -o rules.o rules.cc");
+	Rule *argpcoption = new Rule();
+	argpcoption->addTarget("argpcoption.o");
+	argpcoption->addCommand("g++ -c -Wall -DDEBUG -g3 -o argpcoption.o argpcoption.cc");
 
-		Rule *static_rules = new Rule();
-		static_rules->addTarget("static_rules.o");
-		static_rules->addCommand("g++ -c -Wall -DDEBUG -g3 -o static_rules.o static_rules.cc");
+	Rule *exception = new Rule();
+	exception->addTarget("exception.o");
+	exception->addCommand("g++ -c -Wall -DDEBUG -g3 -o exception.o exception.cc");
 
-		Rule *subprocess_unix = new Rule();
-		subprocess_unix->addTarget("subprocess_unix.o");
-		subprocess_unix->addCommand("g++ -c -Wall -DDEBUG -g3 -o subprocess_unix.o subprocess_unix.cc");
+	Rule *rules = new Rule();
+	rules->addTarget("rules.o");
+	rules->addCommand("g++ -c -Wall -DDEBUG -g3 -o rules.o rules.cc");
+
+	Rule *static_rules = new Rule();
+	static_rules->addTarget("static_rules.o");
+	static_rules->addCommand("g++ -c -Wall -DDEBUG -g3 -o static_rules.o static_rules.cc");
+
+	Rule *subprocess_unix = new Rule();
+	subprocess_unix->addTarget("subprocess_unix.o");
+	subprocess_unix->addCommand("g++ -c -Wall -DDEBUG -g3 -o subprocess_unix.o subprocess_unix.cc");
 }
