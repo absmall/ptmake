bd0ac596e82bf0560d6ea6498722878f1746f840
Aaron Small <a.small@unb.ca>
2011-01-06 22:55:43 -0500

    Comment subprocess.h

--- subprocess.h
+++ subprocess.h
@@ -1,13 +1,20 @@
 #ifndef __SUBPROCESS_H__
 #define __SUBPROCESS_H__
 
-#include <list>
 #include <string>
 
 class Subprocess
 {
 public:
+	/* Execute a command, while calling callbacks on entry and exit to each
+	 * kernel filesystem access
+	 */
 	void trace(std::string command);
+
+	/* Callback when entering a filesystem access */
+	virtual void callback_entry(std::string filename) = 0;
+	
+	/* Callback when leaving a filesystem access */
 	virtual void callback_entry(std::string filename) = 0;
 	virtual void callback_exit(std::string filename, bool success) = 0;
 };
