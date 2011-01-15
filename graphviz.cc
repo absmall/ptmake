e5877c8a78ec10b9dc4ab70bde5b2bbf6087baa2
Aaron Small <a.small@unb.ca>
2011-01-15 18:51:04 -0500

    Fix outstanding issues, graphviz module now works

--- graphviz.cc
+++ graphviz.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "graphviz.h"
 #include "dependencies.h"
 #include "utilities.h"
@@ -7,25 +8,27 @@ using namespace std;
 
 void GraphViz::output(const std::string &filename)
 {
+	ofstream out(filename.c_str(), ios_base::out);
+	
 	list<Rule *>::iterator i;
 	std::list<std::pair<std::string, bool> > *dependencies;
 
-	cout << "digraph G {" << endl;
+	out << "digraph G {" << endl;
 	for( i = Rule::rules.begin(); i != Rule::rules.end(); i ++ ) {
 
 		// Print dependencies
 		dependencies = retrieve_dependencies( (*i)->hash );
 		if( dependencies != NULL ) {
 			for( list<pair<string, bool> >::iterator j = dependencies->begin(); j != dependencies->end(); j ++ ) {
-				cout  << "\""<< j->first  << "\""<< " -> " << printhash((*i)->hash) << endl;
+				out  << "\""<< j->first  << "\" -> \"" << printhash((*i)->hash) << "\"" << endl;
 			}
 			delete dependencies;
 		}
 
 		// Print targets
 		for( list<string>::iterator j = (*i)->targets->begin(); j != (*i)->targets->end(); j ++ ) {
-			cout << printhash((*i)->hash) << " -> " << "\"" << *j << "\"" << endl;
+			out << "\"" << printhash((*i)->hash) << "\" -> \"" << *j << "\"" << endl;
 		}
 	}
-	cout << "}" << endl;
+	out << "}" << endl;
 }
