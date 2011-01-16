b8cbf579c275601b24125334a011dd0a6c5bde33
Aaron Small <a.small@unb.ca>
2011-01-15 19:02:27 -0500

    Missed adding these files earlier

---
+++ utilities.h
@@ -0,0 +1,11 @@
+#ifndef __UTILITIES_H__
+#define __UTILITIES_H__
+
+#include <string>
+
+/*
+ * Convert a hash to a printable form
+ */
+std::string printhash(unsigned char hash[32]);
+
+#endif /* __UTILITIES_H__ */
