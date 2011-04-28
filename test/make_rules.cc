7798cb1ba72cac21fe189bc740f38a7c2ff8c7ec
Aaron Small <a.small@unb.ca>
2011-04-27 20:31:03 -0400

    Update make_rules test to do correct init/deinit

--- test/make_rules.cc
+++ test/make_rules.cc
@@ -5,55 +5,73 @@ Rule *r;
 
 int init_make_rules(void)
 {
-	r = new MakeRule();
 	return 0;
 }
 
 int clean_make_rules(void)
 {
-	delete r;
 	return 0;
 }
 
 void test_make_rules_1(void)
 {
+	r = new MakeRule();
+
 	// Test non-matching regular rule
 	CU_ASSERT( r->match( "hello" ) == false );
+
+	delete r;
 }
 
 void test_make_rules_2(void)
 {
+	r = new MakeRule();
+
 	// Test matching regular rule
 	r->addTarget("hello");
 	CU_ASSERT( r->match( "hello" ) == true );
+
+	delete r;
 }
 
 void test_make_rules_3(void)
 {
+	r = new MakeRule();
+
 	// Test prefix rule
 	r->addTarget("%.hello");
 	CU_ASSERT( r->match( "") == false );
 	CU_ASSERT( r->match( ".hello") == true );
 	CU_ASSERT( r->match( "a.hello") == true );
 	CU_ASSERT( r->match( "a.goodbye") == false );
+
+	delete r;
 }
 
 void test_make_rules_4(void)
 {
+	r = new MakeRule();
+
 	// Test suffix rule
 	r->addTarget("hello%");
 	CU_ASSERT( r->match( "") == false );
 	CU_ASSERT( r->match( "hello") == true );
 	CU_ASSERT( r->match( "hellothere") == true );
 	CU_ASSERT( r->match( "goodbyethere" ) == false );
+
+	delete r;
 }
 
 void test_make_rules_5(void)
 {
+	r = new MakeRule();
+
 	// Test internal wildcard
 	r->addTarget("a%b");
 	CU_ASSERT( r->match( "") == false );
 	CU_ASSERT( r->match( "ab") == true );
 	CU_ASSERT( r->match( "acb") == true );
 	CU_ASSERT( r->match( "bca" ) == false );
+
+	delete r;
 }
